985a3eab38281b88b13ea59c7f42966327c94a6a
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-01 09:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let input = document.getElementById("fname");
+input.addEventListener("blur", onBlur);
+
+function onBlur(){
+    console.log("I am blurred");
+    console.log(input.value);
+    console.log(input.value.toUpperCase());
+	let str = input.value.toUpperCase();
+    input.value = str;
+}
